2a1a74343c4ae6bd1c0997ce428a9f762cd6f710
Lars Vilhuber <lars.vilhuber@cornell.edu>
2019-04-12 20:28:57 -0400

    Trying again

--- bin/sync-up.sh
+++ bin/sync-up.sh
@@ -1,3 +1,3 @@
-ssh -i  /tmp/sftp_rsa vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
+ssh -i  /tmp/sftp_rsa -o "StrictHostKeyChecking=no" vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
 scp -i  /tmp/sftp_rsa cv-vilhuber.pdf vilhuber10@administrator.dreamhost.com:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
 ssh -i  /tmp/sftp_rsa vilhuber10@administrator.dreamhost.com "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
