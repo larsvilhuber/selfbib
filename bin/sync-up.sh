95f672b82bd6f5926841903c299242e400755251
Lars Vilhuber <lars@vilhuber.com>
2022-05-04 11:44:43 -0400

    Update sync-up.sh

--- bin/sync-up.sh
+++ bin/sync-up.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 MYDIR=../web/bib
 DOCS="cv-vilhuber"
+SSHOPTS='-o "StrictHostKeyChecking=no"'
 
 # 
 cd $(dirname $0)/$MYDIR
-ssh -i  /tmp/sftp_rsa -o "StrictHostKeyChecking=no" vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
-scp -i  /tmp/sftp_rsa cv-vilhuber.pdf vilhuber10@administrator.dreamhost.com:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
-ssh -i  /tmp/sftp_rsa vilhuber10@administrator.dreamhost.com "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
+ssh -i  /tmp/sftp_rsa $SSHOPTS vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
+scp -i  /tmp/sftp_rsa $SSHOPTS cv-vilhuber.pdf vilhuber10@administrator.dreamhost.com:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
+ssh -i  /tmp/sftp_rsa $SSHOPTS vilhuber10@administrator.dreamhost.com "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
