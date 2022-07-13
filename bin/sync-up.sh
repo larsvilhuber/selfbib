fe8a8320488abfb644d5afe064061d9e140635ea
Lars Vilhuber <lars.vilhuber@cornell.edu>
2022-07-13 13:37:57 -0400

    [skip travis]Corrected bug in sync script

--- bin/sync-up.sh
+++ bin/sync-up.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 MYDIR=../web/bib
 DOCS="cv-vilhuber"
-SSHOPTS='-o "StrictHostKeyChecking=no"'
+SSHOPTS='-o StrictHostKeyChecking=no'
+
+case $(whoami) in
+  larsvilhuber|lars|vilhuber)
+    KEY=$HOME/.ssh/id_lars2016_rsa
+    ;;
+  *)
+    KEY="/tmp/sftp_rsa"
+    ;;
+esac
 
 # 
 cd $(dirname $0)/$MYDIR
-ssh -i  /tmp/sftp_rsa $SSHOPTS vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
-scp -i  /tmp/sftp_rsa $SSHOPTS cv-vilhuber.pdf vilhuber10@administrator.dreamhost.com:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
-ssh -i  /tmp/sftp_rsa $SSHOPTS vilhuber10@administrator.dreamhost.com "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
+ssh -i $KEY $SSHOPTS vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
+scp -i $KEY $SSHOPTS cv-vilhuber.pdf vilhuber10@administrator.dreamhost.com:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
+ssh -i $KEY $SSHOPTS vilhuber10@administrator.dreamhost.com "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
