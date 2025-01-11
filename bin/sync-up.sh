63b14452d6fe6f4a90bf314b9143b7f25b53c9f4
Lars Vilhuber <lars@vilhuber.com>
2025-01-11 11:42:29 -0500

    Trying to fix the push problems

--- bin/sync-up.sh
+++ bin/sync-up.sh
@@ -17,4 +17,4 @@ esac
 cd $(dirname $0)/$MYDIR
 ssh -i $KEY $SSHOPTS vilhuber10@$REMOTEHOST "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
 scp -i $KEY $SSHOPTS cv-vilhuber.pdf vilhuber10@$REMOTEHOST:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
-ssh -i $KEY $SSHOPTS vilhuber10@$REMOTEHOST "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
+ssh -i $KEY $SSHOPTS vilhuber10@$REMOTEHOST "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/selfbib && git pull"
