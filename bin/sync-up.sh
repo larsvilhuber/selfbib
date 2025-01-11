f51af191db1856986d91a6a9f73cee256608bcf8
Lars Vilhuber <lars@vilhuber.com>
2025-01-10 22:04:27 -0500

    Updating build process

--- bin/sync-up.sh
+++ bin/sync-up.sh
@@ -2,6 +2,7 @@
 MYDIR=../web/bib
 DOCS="cv-vilhuber"
 SSHOPTS='-o StrictHostKeyChecking=no'
+REMOTEHOST=ssh.vilhuber.com
 
 case $(whoami) in
   larsvilhuber|lars|vilhuber)
@@ -14,6 +15,6 @@ esac
 
 # 
 cd $(dirname $0)/$MYDIR
-ssh -i $KEY $SSHOPTS vilhuber10@administrator.dreamhost.com "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
-scp -i $KEY $SSHOPTS cv-vilhuber.pdf vilhuber10@administrator.dreamhost.com:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
-ssh -i $KEY $SSHOPTS vilhuber10@administrator.dreamhost.com "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
+ssh -i $KEY $SSHOPTS vilhuber10@$REMOTEHOST "rm /home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/cv-vilhuber.pdf"
+scp -i $KEY $SSHOPTS cv-vilhuber.pdf vilhuber10@$REMOTEHOST:/home/vilhuber10/www.vilhuber.com/lars/wp-content/uploads/c/
+ssh -i $KEY $SSHOPTS vilhuber10@$REMOTEHOST "cd /home/vilhuber10/www.vilhuber.com/lars/wp-content/papercite-data && svn up"
